1b9eb9494b48c6d4b436b7a0bdee156ed2cb7526
GustavolLo <goodluke111931@gmail.com>
2020-12-29 17:19:54 +0100

    Adding icons in contact page.

--- src/components/contact/ContactPage.js
+++ src/components/contact/ContactPage.js
@@ -1,18 +1,43 @@
 import React from "react";
+import "./ContactPage.css";
 
 export default function ContactPage() {
   return (
-    <div>
-      <h1>Contactez nous!</h1>
-      <div>
-        <h3>Facebook</h3>
-        <span>https://www.facebook.com/americaandinafr</span>
-        <h3>Instagram</h3>
-        <span>https://www.instagram.com/americaandinafr/?hl=fr</span>
-        <h3>E-mail</h3>
-        <span>america.andina.fr@gmail.com</span>
-        <h3>Téléphone</h3>
-        <span>+33 6 73 29 22 27</span>
+    <div className="container">
+      <h1 className="display-2 my-5 pt-2">Contactez nous!</h1>
+      <div className="row">
+        <div className="col">
+          <div className="inline">
+            <span className="icon facebook"></span>
+            <h3 className="display-5">Facebook</h3>
+            <a href="https://www.facebook.com/americaandinafr">
+              https://www.facebook.com/americaandinafr
+            </a>
+          </div>
+          <div className="inline">
+            <span className="icon instagram"></span>
+            <h3 className="display-5">Instagram</h3>
+            <a
+              href="https://www.instagram.com/americaandinafr/?hl=fr"
+              rel="noreferrer"
+              target="_blank"
+            >
+              https://www.instagram.com/americaandinafr/?hl=fr
+            </a>
+          </div>
+          <div className="inline">
+            <span className="icon gmail"></span>
+            <h3>E-mail</h3>
+            <a href="mailto: america.andina.fr@gmail.com">
+              america.andina.fr@gmail.com
+            </a>
+          </div>
+          <div className="inline">
+            <span className="icon whatsapp"></span>
+            <h3>Téléphone</h3>
+            <a href="tel:+33 6 73 29 22 27">+33 6 73 29 22 27</a>
+          </div>
+        </div>
       </div>
     </div>
   );
